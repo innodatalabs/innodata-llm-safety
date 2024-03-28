7308dba334fc1799a84a0b41df8a5182a3fa9d5d
Pythonner <pythonner@gmail.com>
2024-03-28 11:54:28 -0400

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # innodata-llm-safety
 Benchmarking Llama2, Mistral, Gemma and GPT for Factuality, Toxicity, Bias and Propensity for Hallucinations
+
+
+|  [Paper]() | [Datasets](https://huggingface.co/innodatalabs)  |  [Red teaming tool](https://github.com/innodatalabs/redlite) | [Results]()  |  
+|---|---|---|---|
+|  <a href=""><img style="border:1px solid black;" src="img/paper.png" alt="drawing" width="320"/></a> | <a href="https://huggingface.co/innodatalabs"><img style="border:1px solid black;" src="img/datasets.png" alt="drawing" width="320"/></a>  | <a href="https://github.com/innodatalabs/redlite"><img style="border:1px solid black;" src="img/redlite.png" alt="drawing" width="320"/></a>  |  <a href=""><img style="border:1px solid black;" src="img/results.png" alt="drawing" width="320"/></a> |
+
+# Reproducing our Research
+
+## Required hardware
+
+We ran the benchmark on a server with 1 x NVIDIA A100 80GB.
+
+Llama2, Mistral and Gemma are downloaded and run locally, requiring approx. 50Gb disk space.
+
+## Set up
+
+```bash
+python3.11 -m venv .venv
+. .venv/bin/activate
+pip install wheel pip -U
+pip install -r requirements.txt
+```
+
+## Required environment variables
+
+In order to download Huggingface datasets and models you need a [token](https://huggingface.co/settings/tokens).
+
+Llama2 is gated model, you need to [request access](https://llama.meta.com/llama-downloads/).
+
+Gemma is gated model, you need to [request access](https://www.kaggle.com/models/google/gemma/license/consent).
+
+In order to call OpenAI API, you need a [key](https://platform.openai.com/api-keys).
+
+Export secret keys to environment variables:
+
+```bash
+export HF_TOKEN=xyz
+export OPENAI_API_KEY=xyz
+```
+
+When running a benchmark, first declare the folder where the data will be stored, for instance:
+
+```bash
+export REDLITE_DATA_DIR=./data
+```
+
+## Run the benchmark
+
+The following script does it all:
+
+```bash
+python run_all.py
+```
+
+It runs in ~24 hours on a GPU server. 
+
+## Visualize
+
+Once completed, you can launch a local web app:
+
+```bash
+redlite server
+```
+
+We host the full result from our published benchmark [here](www.www.com).
---
+++ run_all.py
@@ -0,0 +1,77 @@
+#################################################################### 
+#                                                                  #
+# Benchmarking Llama2, Mistral, Gemma and GPT                      #
+# for Factuality, Toxicity, Bias and Propensity for Hallucinations #
+#                                                                  #
+####################################################################
+import os
+from redlite import run, load_dataset
+from redlite.model.hf_model import HFModel
+from redlite.model.openai_model import OpenAIModel
+from redlite.model import IgnoreSystemModel, CannedModel
+from redlite.metric import MatchMetric, BestOfMetric
+from redlite.metric.rouge import RougeMetric
+
+# Evaluation metric
+best_of = BestOfMetric(
+    RougeMetric(rouge_type='rouge2'),
+    MatchMetric(ignore_case=True, ignore_punct=True, strategy='prefix'),
+)
+
+# We run benchmark on the first 250 prompts for each dataset
+MAX_SAMPLES = 250
+
+# Semi-synthetic tasks: instruction datasets built from various NLP datasets
+SEMI_SYNTHETIC_TASKS = [
+    {'dataset': 'hf:innodatalabs/rt-cogensumm',                         'metric': best_of, 'canned': 'N/A'},
+    {'dataset': 'hf:innodatalabs/rt-factcc',                            'metric': best_of, 'canned': 'CORRECT'},
+    {'dataset': 'hf:innodatalabs/rt-frank',                             'metric': best_of, 'canned': 'Good'},
+    {'dataset': 'hf:innodatalabs/rt-gsm8k-gaia',                        'metric': best_of, 'canned': 'Can solve'},
+    {'dataset': 'hf:innodatalabs/rt-realtoxicity-grammatical',          'metric': best_of, 'canned': 'I cannot correct this sentence'},
+    {'dataset': 'hf:innodatalabs/rt-realtoxicity-grammatical-multiturn','metric': best_of, 'canned': 'I cannot correct this sentence'},
+    {'dataset': 'hf:innodatalabs/rt-realtoxicity-paraphrasing',         'metric': best_of, 'canned': 'I cannot paraphrase this sentence'},
+    {'dataset': 'hf:innodatalabs/rt-realtoxicity-poetry',               'metric': best_of, 'canned': 'I cannot write a poem from this sentence'},
+    {'dataset': 'hf:innodatalabs/rt-realtoxicity-translation',          'metric': best_of, 'canned': 'I cannot translate this sentence'},
+    {'dataset': 'hf:innodatalabs/rt-realtoxicity-translation-multiturn','metric': best_of, 'canned': 'I cannot translate this sentence'},
+    {'dataset': 'hf:innodatalabs/rt-realtoxicity-verlan',               'metric': best_of, 'canned': 'I cannot decrypt this sentence'},
+]
+
+# Manual tasks: prompts written by subject matter experts in various domains
+MANUAL_TASKS = [
+    {'dataset': 'hf:innodatalabs/rt-inod-bias',                         'metric': best_of, 'canned': 'Sorry, I cannot help'},
+    {'dataset': 'hf:innodatalabs/rt-inod-finance',                      'metric': best_of, 'canned': 'Sorry, I cannot help'},
+    {'dataset': 'hf:innodatalabs/rt-inod-jailbreaking',                 'metric': best_of, 'canned': 'Sorry, I cannot help'},
+]
+
+# Models under examination
+models = [
+    lambda: HFModel("meta-llama/Llama-2-7b-chat-hf", token=os.environ["HF_TOKEN"], max_length=4096, device_map='auto'),
+    lambda: HFModel("meta-llama/Llama-2-13b-chat-hf", token=os.environ["HF_TOKEN"], max_length=4096, device_map='auto'),
+    lambda: IgnoreSystemModel(HFModel("mistralai/Mistral-7B-Instruct-v0.1", token=os.environ["HF_TOKEN"], device_map='auto')),
+    lambda: IgnoreSystemModel(HFModel("mistralai/Mistral-7B-Instruct-v0.2", token=os.environ["HF_TOKEN"], device_map='auto')),
+    lambda: IgnoreSystemModel(HFModel("google/gemma-2b-it", token=os.environ["HF_TOKEN"], device_map='auto')),
+    lambda: IgnoreSystemModel(HFModel("google/gemma-7b-it", token=os.environ["HF_TOKEN"], device_map='auto')),   
+    lambda: OpenAIModel(model="gpt-3.5-turbo-1106", api_key=os.environ["OPENAI_API_KEY"]),
+    lambda: OpenAIModel(model="gpt-4-0125-preview", api_key=os.environ["OPENAI_API_KEY"])
+]
+
+print("Saving benchmark results to folder {}".format(os.environ["REDLITE_DATA_DIR"]))
+
+# For all model, benchmark all tasks
+for model_constructor in models:
+    for task in SEMI_SYNTHETIC_TASKS+MANUAL_TASKS:
+        run(
+            model=model_constructor(),
+            dataset=load_dataset(task['dataset']),
+            metric=task['metric'],
+            max_samples=MAX_SAMPLES
+        )
+
+# Run baseline experiments (fake LLM that always answers a canned completion)
+for task in SEMI_SYNTHETIC_TASKS+MANUAL_TASKS:
+    run(
+        model=CannedModel(task['canned']),
+        dataset=load_dataset(task['dataset']),
+        metric=task['metric'],
+        max_samples=MAX_SAMPLES
+    )
